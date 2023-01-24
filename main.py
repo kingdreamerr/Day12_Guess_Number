8665c095d5567a260736e6195d858176e9af7dfc
kingdreamerr <jackmvula458@gmail.com>
2023-01-24 16:12:21 +0200

    Add game logic

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ if level_chosen == 'hard':
   lives = 5
 else:
   lives = 10
+while not guessed_right_number and lives != 0:
+  print(f"you have {lives} attempts remaining to guess the number")
+  guess = int(input("Make a guess: "))
+    
+  if guess == guess_number:
+    print("You guessed the right number!")
+    guessed_right_number = True
+  elif guess < guess_number:
+    lives -=1
+    print("Too low")
+  elif guess > guessed_right_number:
+    lives -=1
+    print("Too high")
+  elif lives == 0:
+    print("You have {lives} you lose")
