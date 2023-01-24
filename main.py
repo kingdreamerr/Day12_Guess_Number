c7c5adb5dadb095d2750361a0e7f1dc0bad07113
kingdreamerr <jackmvula458@gmail.com>
2023-01-24 16:12:03 +0200

    Add the needed variables and get user input

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from random import randint
 from art import logo
 
 print(logo)
+guess_number = randint(0,101)
+lives = 0
+level = ""
+guessed_right_number = False
+
+print("Welcome to the guessing game!")
+print("I'm guessing a number between 1 and 100.")
+level_chosen = input("Choose a difficulty. type 'easy' or 'hard': ").lower()
+if level_chosen == 'hard':
+  lives = 5
+else:
+  lives = 10
